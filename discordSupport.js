94b74c429807350565523605bbec6683dc72548e
Matt Carroll <admin@develogix.com>
2017-08-30 21:38:59 -0400

    resolves #5

--- discordSupport.js
+++ discordSupport.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 
 exports.discordLogin = function (Kirbi) {
-	const Discord = require("discord.js");
+	const Discord = require('discord.js');
 	Kirbi.Discord = new Discord.Client();
 	console.log(chalk.magenta(`Discord Enabled... Starting.\nDiscord.js version: ${Discord.version}`));
 	if (Kirbi.Auth.discord.bot_token) {
@@ -10,20 +10,24 @@ exports.discordLogin = function (Kirbi) {
 	} else {
 		console.log(chalk.red('ERROR: Kirbi must have a Discord bot token...'));
 	};
+
 	require('./antiraid')(Kirbi);
 	require('./lib/onEvent')(Kirbi);
 
 	//Load external discord-specific modules
 	if (Kirbi.Config.discord.modules.length && Kirbi.Config.discord.modules instanceof Array) {
 		Kirbi.discordCommands = {};
+
 		Kirbi.Config.discord.modules.forEach(module => {
-			if (Kirbi.discordCommands[module]) {return};
+			if (Kirbi.discordCommands[module]) { return; }
+
 			try {
 				module = require(`kirbi-discord-${module}`)(Kirbi);
 			} catch (err) {
 				console.log(chalk.red(`Improper setup of the 'discord-${module}' command file. : ${err}`));
 				return;
 			}
+
 			if (module && module['commands']) {
 				module.commands.forEach(command => {
 					if (command in module) {

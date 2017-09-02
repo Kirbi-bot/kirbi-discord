4efbc70a82c071c5b037488c7d3cd8a8c2281b96
Nate Richardson <hello@naterichardson.com>
2017-09-01 22:13:17 -0400

    feexes

--- discord-support.js
+++ discord-support.js
@@ -7,18 +7,17 @@ exports.discordLogin = function (Kirbi) {
 	if (Kirbi.Auth.discord.bot_token) {
 		console.log('Logging in to Discord...');
 		Kirbi.Discord.login(Kirbi.Auth.discord.bot_token);
+		require('./antiraid')(Kirbi);
+		require('./lib/on-event')(Kirbi);
 	} else {
 		console.log(chalk.red('ERROR: Kirbi must have a Discord bot token...'));
+		return;
 	}
-
-	require('./antiraid')(Kirbi);
-	require('./lib/on-event')(Kirbi);
 
 	Kirbi.setupDiscordCommands = function () {
 		// Load external discord-specific modules
 		if (Kirbi.Config.discord.modules.length > 0 && Array.isArray(Kirbi.Config.discord.modules)) {
 			Kirbi.discordCommands = {};
-
 			Kirbi.Config.discord.modules.forEach(module => {
 				if (Kirbi.discordCommands[module]) {
 					return;

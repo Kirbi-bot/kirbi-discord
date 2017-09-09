95640df934de53f9b2109698d14c944e5bb6a3e7
KnightVision90 <joseph.moraniii@gmail.com>
2017-09-09 15:23:44 -0400

    Resolves #31
    Removes antiraid functionality that has been moved to it's own module.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A module for [Kirbi](https://github.com/richardson-media-house/kirbi), that adds
 
 ## Sub-modules
 
+[kirbi-discord-antiraid](https://github.com/Richardson-Media-House/kirbi-discord-moderation) => prevents raids on discord servers and handles welcoming of new members.
+
 [kirbi-discord-moderation](https://github.com/Richardson-Media-House/kirbi-discord-moderation) => adds various server moderation commands.
 
 [kirbi-discord-musicplayer](https://github.com/Richardson-Media-House/kirbi-discord-musicplayer) => adds support playing music in voice channels, and associated commands.
--- discord-support.js
+++ discord-support.js
@@ -7,45 +7,44 @@ exports.discordLogin = function (Kirbi) {
 	if (Kirbi.Auth.discord.bot_token) {
 		console.log('Logging in to Discord...');
 		Kirbi.Discord.login(Kirbi.Auth.discord.bot_token);
-		require('./antiraid')(Kirbi);
 		require('./lib/on-event')(Kirbi);
 	} else {
 		console.log(chalk.red('ERROR: Kirbi must have a Discord bot token...'));
 		return;
 	}
 
-	Kirbi.setupDiscordCommands = function () {
-		// Load external discord-specific modules
-		if (Kirbi.Config.discord.modules.length > 0 && Array.isArray(Kirbi.Config.discord.modules)) {
-			Kirbi.discordCommands = {};
-			Kirbi.Config.discord.modules.forEach(module => {
-				if (Kirbi.discordCommands[module]) {
-					return;
-				}
+Kirbi.setupDiscordCommands = function () {
+	// Load external discord-specific modules
+	if (Kirbi.Config.discord.modules.length > 0 && Array.isArray(Kirbi.Config.discord.modules)) {
+		Kirbi.discordCommands = {};
+		Kirbi.Config.discord.modules.forEach(module => {
+			if (Kirbi.discordCommands[module]) {
+				return;
+			}
 
-				try {
-					module = require(`kirbi-discord-${module}`)(Kirbi);
-				} catch (err) {
-					console.log(chalk.red(`Improper setup of the 'discord-${module}' command file. : ${err}`));
-					return;
-				}
+			try {
+				module = require(`kirbi-discord-${module}`)(Kirbi);
+			} catch (err) {
+				console.log(chalk.red(`Improper setup of the 'discord-${module}' command file. : ${err}`));
+				return;
+			}
 
-				if (module && module.commands) {
-					module.commands.forEach(command => {
-						if (command in module) {
-							try {
-								Kirbi.discordCommands[command] = module[command];
-							} catch (err) {
-								console.log(err);
-							}
+			if (module && module.commands) {
+				module.commands.forEach(command => {
+					if (command in module) {
+						try {
+							Kirbi.discordCommands[command] = module[command];
+						} catch (err) {
+							console.log(err);
 						}
-					});
-				}
-			});
-		}
-	};
-	Kirbi.setupDiscordCommands();
+					}
+				});
+			}
+		});
+	}
+};
+Kirbi.setupDiscordCommands();
 
-	console.log(`Loaded ${Kirbi.commandCount()} base commands`);
-	console.log(`Loaded ${Object.keys(Kirbi.discordCommands).length} Discord commands`);
+console.log(`Loaded ${Kirbi.commandCount()} base commands`);
+console.log(`Loaded ${Object.keys(Kirbi.discordCommands).length} Discord commands`);
 };
